fdfc322228ccfae6ca6b05f5b048c1152728b011
Caleb Miller <calebgmiller@gmail.com>
2019-02-03 22:11:41 -0700

    Add files via upload

---
+++ Week 4/Inventory/Inventory/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+
+    public class drinks : Form1, Inventory
+    {
+        public void JumpToProd(int x, int y);
+    }
+
+    public void Clear()
+    {
+        throw new System.Exception("Not yet created");
+    }
+
+    public void Update()
+    {
+        throw new System.Exception("Not yet created");
+    }
+
+    private String name;
+
+    public string name()
+    {
+        throw new System.Exception("Not yet created");
+    }
+
+
+}
